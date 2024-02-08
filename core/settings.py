ec38e7570cd3f40d1a239a3294ca16d411ffad85
makhmudjonfoziljonov <mahmudjonfoziljonv1@gmail.com>
2024-02-08 10:38:31 +0500

    added static_url

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,6 +120,8 @@ STATICFILES_DIRS = [
     "/var/www/static/",
 ]
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
